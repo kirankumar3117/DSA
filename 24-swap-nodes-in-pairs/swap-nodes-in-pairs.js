e0eda9667b6b453526aad3cc367b75ab6f039f7b
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-06-20 07:48:30 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (52.94%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -10,22 +10,33 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    if(!head || !head.next) return head;
-    let dummy =new ListNode();
-    dummy.next = head;
+    // if(!head || !head.next) return head;
+    // let dummy =new ListNode();
+    // dummy.next = head;
 
-    let p = dummy;
-    let c = head;
-    let n = head.next;
+    // let p = dummy;
+    // let c = head;
+    // let n = head.next;
     
-    while(c && n){
-        p.next = n;
-        c.next = n.next;
-        n.next = c
+    // while(c && n){
+    //     p.next = n;
+    //     c.next = n.next;
+    //     n.next = c
 
-        p = c;
-        c = p.next;
-        n = c && c.next
-    }
-    return dummy.next;
+    //     p = c;
+    //     c = p.next;
+    //     n = c && c.next
+    // }
+    // return dummy.next;
+
+    // Recursive approach
+    if(!head || !head.next) return head;
+    
+    let l = head;
+    let r = head.next;
+
+    l.next = swapPairs(r.next)
+    r.next = l;
+
+    return r
 };
