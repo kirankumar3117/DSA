23eb0ec4b534e7abe8cc46702610b9a03a2f313e
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-06-28 13:34:56 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (44.87%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -8,7 +8,7 @@
 var merge = function(nums1, m, nums2, n) {
    // Approach 1
    // By taking nums1 as copy
-   const n1Copy = nums1.slice(0,m);
+   /*const n1Copy = nums1.slice(0,m);
    let p1=0, p2 = 0;
    for(let i=0;i<m+n;i++){
     if((p1<m && n1Copy[p1] < nums2[p2]) || p2>=n){
@@ -17,6 +17,18 @@ var merge = function(nums1, m, nums2, n) {
     }else{
         nums1[i] = nums2[p2];
         p2++
+    }
+   }*/
+   //Approach Two
+   let p1=m-1, p2 = n-1;
+   for(let i=m+n-1;i>=0;i--){
+    if(p2 <0) break;
+    if(p1>=0 && nums1[p1] > nums2[p2]){
+        nums1[i] = nums1[p1]
+        p1--
+    }else{
+        nums1[i] = nums2[p2] 
+        p2--
     }
    }
 };
