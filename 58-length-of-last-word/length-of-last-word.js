71010a380b41089fa8b4b9ab652eb1a1d1e74d02
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-10 07:03:09 +0530

    Time: 0 ms (100.00%) | Memory: 52.9 MB (89.12%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,15 +3,26 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    let n = s.length-1;
-
-    while(s[n] === " "){
-        n=n-1;
-    }
+    //Approach 1 (not for interview)
+    // s = s.trim()
+    // s = s.split(" ");
+    // return s[s.length-1].length
+    // Approach 2
+    // let n = s.length-1;
+    // while(n>=0 && s[n] === " ") n--;
+    // let count = 0;
+    // while(n>=0 && s[n] !== " "){
+    //     n--;
+    //     count++;
+    // }
+    // return count;
+    //Approach3
+    let n = s.length - 1;
     let count = 0;
-    while(s[n] != " " && n >=0){
-        count++;
-        n=n-1
+    while(n>=0){
+        if(s[n] != " ") count++;
+        else if(count>0) return count;
+        n--
     }
-    return count
+    return count;
 };
