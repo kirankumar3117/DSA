9dca69799309f4f2365f904c5592c737b686988f
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-11 22:29:32 +0530

    Time: 3 ms (58.66%) | Memory: 65.2 MB (52.05%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = prices[0]
+    let profit = 0;
+    for(let i=1; i< prices.length;i++){
+        if(prices[i] - min > profit){
+            profit = prices[i] - min
+        }
+        if(prices[i] < min){
+            min = prices[i]
+        }
+    }
+    return profit;
+}
