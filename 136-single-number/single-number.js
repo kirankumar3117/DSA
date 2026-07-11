a828dc352ad3b02dc90b511730e2026db9e26640
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-12 01:02:18 +0530

    Time: 1 ms (71.60%) | Memory: 54.2 MB (95.63%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -4,7 +4,7 @@
  */
 var singleNumber = function(nums) {
    // Approach 1
-   let hash = {};
+   /*let hash = {};
    for(let num of nums){
      hash[num] = (hash[num] || 0) + 1;
    }
@@ -12,5 +12,12 @@ var singleNumber = function(nums) {
      if(hash[num] === 1){
         return +num
      }
-   }
+   }*/
+    // Approach 2
+    let result = 0;
+    for(let i=0;i<nums.length;i++){
+        result=result ^ nums[i];
+    }
+    return result;
+
 };
