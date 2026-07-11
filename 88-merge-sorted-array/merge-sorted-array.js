953a389ccc4d11231d0643b3254ca14c21d0aedf
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-11 22:42:53 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (72.00%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -8,27 +8,27 @@
 var merge = function(nums1, m, nums2, n) {
    // Approach 1
    // By taking nums1 as copy
-   /*const n1Copy = nums1.slice(0,m);
-   let p1=0, p2 = 0;
+   const n1copy = nums1.slice(0,m);
+   let p1 = 0, p2 = 0;
    for(let i=0;i<m+n;i++){
-    if((p1<m && n1Copy[p1] < nums2[p2]) || p2>=n){
-        nums1[i] = n1Copy[p1]
-        p1++;
-    }else{
+     if((n1copy[p1] < nums2[p2] && p1 < m) || p2>=n){
+        nums1[i] = n1copy[p1]
+        p1++
+     }else{
         nums1[i] = nums2[p2];
         p2++
-    }
-   }*/
-   //Approach Two
-   let p1=m-1, p2 = n-1;
-   for(let i=m+n-1;i>=0;i--){
-    if(p2 <0) break;
-    if(p1>=0 && nums1[p1] > nums2[p2]){
-        nums1[i] = nums1[p1]
-        p1--
-    }else{
-        nums1[i] = nums2[p2] 
-        p2--
-    }
+     }
    }
+   //Approach Two
+    //    let p1=m-1, p2 = n-1;
+    //    for(let i=m+n-1;i>=0;i--){
+    //     if(p2 <0) break;
+    //     if(p1>=0 && nums1[p1] > nums2[p2]){
+    //         nums1[i] = nums1[p1]
+    //         p1--
+    //     }else{
+    //         nums1[i] = nums2[p2] 
+    //         p2--
+    //     }
+    //    }
 };
