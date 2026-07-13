bb9af87ed9d2eefb2cba4e5403cbd04ce7ff6860
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-13 10:23:18 +0530

    Time: 7 ms (66.76%) | Memory: 58.2 MB (27.70%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -3,19 +3,32 @@
  * @return {number}
  */
 var maxFreqSum = function(s) {
-    const freq = {}
+    const freq = {};
     const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    let mxVowels = 0;
+    let mxCons = 0;
     for(let ch of s){
         freq[ch] = (freq[ch] || 0) + 1;
-    }
-    let vCount = 0;
-    let cCount = 0;
-    for(let ch in freq){
         if(vowels.has(ch)){
-            vCount = Math.max(freq[ch], vCount)
+            mxVowels = Math.max(mxVowels, freq[ch])
         }else{
-            cCount = Math.max(freq[ch], cCount)
+            mxCons = Math.max(mxCons, freq[ch])
         }
     }
-    return vCount + cCount;
+    return mxVowels + mxCons;
+    // const freq = {}
+    // const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
+    // for(let ch of s){
+    //     freq[ch] = (freq[ch] || 0) + 1;
+    // }
+    // let vCount = 0;
+    // let cCount = 0;
+    // for(let ch in freq){
+    //     if(vowels.has(ch)){
+    //         vCount = Math.max(freq[ch], vCount)
+    //     }else{
+    //         cCount = Math.max(freq[ch], cCount)
+    //     }
+    // }
+    // return vCount + cCount;
 };
