34a390e65129e0f6ef6a222ddcf37ffed0d5d4c3
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-13 09:30:58 +0530

    Time: 1 ms (41.78%) | Memory: 54.1 MB (55.39%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -18,14 +18,25 @@ var numJewelsInStones = function(jewels, stones) {
     // }
     // return count;
 
-    let set = new Set();
-    for(let i=0;i<jewels.length;i++){
-        set.add(jewels[i])
-    };
+    // let set = new Set();
+    // for(let i=0;i<jewels.length;i++){
+    //     set.add(jewels[i])
+    // };
+    // let count = 0;
+    // for(let i=0;i<stones.length;i++){
+    //     if(set.has(stones[i])){
+    //         count++;
+    //     }
+    // }
+    // return count;
+
     let count = 0;
     for(let i=0;i<stones.length;i++){
-        if(set.has(stones[i])){
-            count++;
+        for(let j=0;j<jewels.length;j++){
+            if(jewels[j] === stones[i]){
+                count++;
+                break;
+            }
         }
     }
     return count;
