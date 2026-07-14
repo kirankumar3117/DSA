b1ebd78004b17b6c2b06c2b3938e2976ac97b38a
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-14 14:24:23 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (43.15%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 var balancedStringSplit = function(s) {
-    let balCount = 0, rCount = 0, lCount = 0;
-    for(let ch of s){
-        if(ch==='R') rCount++;
-        else lCount++;
-        if(rCount === lCount){
-            balCount++;
-            rCount = lCount = 0;
+    let balance = 0;
+    let count = 0;
+
+    for (const ch of s) {
+        balance += ch === 'R' ? 1 : -1;
+
+        if (balance === 0) {
+            count++;
         }
     }
-    return balCount;
+
+    return count;
 };
