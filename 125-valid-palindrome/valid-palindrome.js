00daedebdb44a437b42019512889a8d3aa3b870e
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-15 10:59:13 +0530

    Time: 7 ms (37.22%) | Memory: 63.8 MB (7.42%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -7,7 +7,9 @@ var isPalindrome = function(s) {
     let filteredStr = "";
     let reverse = ""
     for(let i=0; i<s.length;i++){
-        if((s[i].charCodeAt() >= 'a'.charCodeAt() && s[i].charCodeAt() <= 'z'.charCodeAt()) || (s[i].charCodeAt() >= '0'.charCodeAt() && s[i].charCodeAt() <= '9'.charCodeAt())){
+        let ch = s[i].charCodeAt();
+        if((ch >= 97 && ch <= 122) || (ch >= 48 && ch <= 57)){
+        // if((s[i].charCodeAt() >= 'a'.charCodeAt() && s[i].charCodeAt() <= 'z'.charCodeAt()) || (s[i].charCodeAt() >= '0'.charCodeAt() && s[i].charCodeAt() <= '9'.charCodeAt())){
         // if(s[i].match(/[a-z0-9]/i)){
             filteredStr = filteredStr + s[i]
             reverse = s[i] + reverse;
