3542e4f271dec2d7d9ee1b313c7169d478627a84
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-15 11:33:38 +0530

    Time: 0 ms (100.00%) | Memory: 56.4 MB (84.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,20 +2,23 @@
  * @param {string} s
  * @return {boolean}
  */
+ function isAlphaNumeric(ch){
+    return ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9'))
+ }
 var isPalindrome = function(s) {
     s = s.toLowerCase();
     let i=0;
     let j=s.length-1;
     while(i<j){
-        if(!s[i].match(/[a-z0-9]/i)){
+        if(!isAlphaNumeric(s[i])){
             i++
-        }else if(!s[j].match(/[a-z0-9]/i)){
+        }else if(!isAlphaNumeric(s[j])){
             j--
-        }else if(s[i] === s[j]){
-            i++;
-            j--;
+        }else if(s[i] != s[j]){
+            return false
         }else{
-            return false;
+            i++
+            j--
         }
     }
     return true;
