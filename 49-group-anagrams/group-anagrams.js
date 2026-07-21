521d768721db9182b5ee30bdbb1fc29cff620d03
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-21 19:23:49 +0530

    Time: 48 ms (14.65%) | Memory: 69.2 MB (19.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,13 +3,21 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    let map = {};
+    let map = {}
     for(let i=0;i<strs.length;i++){
-        let str = strs[i].split("").sort().join("");
-        if(map[str] != undefined){
-           map[str].push(strs[i])
+        let str = strs[i]
+        let freqArr = Array(26).fill(0)
+        for(let j=0;j<str.length;j++){
+            freqArr[str[j].charCodeAt() - 97] +=1;
+        }
+        let key = ""
+        for(let k=0;k<26;k++){
+            key = key + String.fromCharCode(k) + freqArr[k]
+        }
+        if(!map[key]){
+            map[key] = [strs[i]]
         }else{
-            map[str] = [strs[i]]
+            map[key].push(strs[i])
         }
     }
    return [...Object.values(map)]
