495501238854e3f512fe0a0143f96b738aea3898
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-21 15:18:23 +0530

    Time: 4 ms (90.90%) | Memory: 54.6 MB (78.51%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -4,15 +4,29 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    if(s.length != t.length) return false;
-    let hash = {};
-    let nHash = {}
-    for(let i=0;i<s.length;i++){
-        if((hash[s[i]] && hash[s[i]] != t[i]) || (nHash[t[i]] && nHash[t[i]] != s[i])){
-           return false;
+    // if(s.length != t.length) return false;
+    // let hash = {};
+    // let nHash = {}
+    // for(let i=0;i<s.length;i++){
+    //     if((hash[s[i]] && hash[s[i]] != t[i]) || (nHash[t[i]] && nHash[t[i]] != s[i])){
+    //        return false;
+    //     }
+    //     hash[s[i]] = t[i];
+    //     nHash[t[i]] = s[i]
+    // };
+    // return true;
+
+      const mapS = {};
+    const mapT = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (mapS[s[i]] !== mapT[t[i]]) {
+            return false;
         }
-        hash[s[i]] = t[i];
-        nHash[t[i]] = s[i]
-    };
+
+        mapS[s[i]] = i + 1;
+        mapT[t[i]] = i + 1;
+    }
+
     return true;
 };
