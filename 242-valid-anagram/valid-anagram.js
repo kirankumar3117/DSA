5c486fca8f3a44d1a1957de0ee95dcf647eca40e
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-21 12:06:21 +0530

    Time: 6 ms (93.80%) | Memory: 55.1 MB (86.65%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,15 +4,28 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
+    // if(s.length != t.length) return false;
+    // let hash = {}
+    // for(let i=0;i<s.length;i++){
+    //     hash[s[i]] = (hash[s[i]] || 0) + 1; 
+    //     hash[t[i]] = (hash[t[i]] || 0 ) - 1;
+    // }
+    // for(let ch in hash){
+    //     if(hash[ch] != 0){
+    //         return false
+    //     }
+    // }
+    // return true;
     if(s.length != t.length) return false;
-    let hash = {}
+    let count = new Array(26).fill(0);
     for(let i=0;i<s.length;i++){
-        hash[s[i]] = (hash[s[i]] || 0) + 1;
-        hash[t[i]] = (hash[t[i]] || 0 ) - 1;
+        count[s.charCodeAt(i) - 97]++;
+        count[t.charCodeAt(i) - 97]--;
     }
-    for(let ch in hash){
-        if(hash[ch] != 0){
-            return false
+
+    for(let num of count){
+        if(num != 0){
+            return false;
         }
     }
     return true;
