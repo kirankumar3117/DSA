5c6dad40ebb62e476e31457bd12c2d4a3a34ba32
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-21 21:49:43 +0530

    Time: 31 ms (60.97%) | Memory: 69.4 MB (16.62%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,31 +3,31 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    let map = {};
+    // let map = {};
+    // for(let i=0;i<strs.length;i++){
+    //     let str = strs[i].split("").sort().join("");
+    //     if(!map[str]){
+    //         map[str] = []
+    //     };
+    //     map[str].push(strs[i])
+    // };
+    // return Object.values(map)
+
+    let map = {}
     for(let i=0;i<strs.length;i++){
-        let str = strs[i].split("").sort().join("");
-        if(!map[str]){
-            map[str] = []
-        };
-        map[str].push(strs[i])
-    };
-    return Object.values(map)
 
-//     let map = {}
-//     for(let i=0;i<strs.length;i++){
+        let freqArr = new Array(26).fill(0)
+        for(let ch of strs[i]){
+            freqArr[ch.charCodeAt() - 97]++;
+        }
 
-//         let freqArr = new Array(26).fill(0)
-//         for(let ch of strs[i]){
-//             freqArr[ch.charCodeAt() - 97]++;
-//         }
+        let key = freqArr.join("#")
+        if(!map[key]){
+            map[key] = []
+        }
 
-//         let key = freqArr.join("#")
-//         if(!map[key]){
-//             map[key] = []
-//         }
-
-//         map[key].push(strs[i])
+        map[key].push(strs[i])
         
-//     }
-//    return Object.values(map)
+    }
+   return Object.values(map)
 };
