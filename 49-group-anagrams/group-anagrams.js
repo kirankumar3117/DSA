da26fda77d9a12bab29220a0e097397adffe01c7
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-21 16:22:08 +0530

    Time: 38 ms (34.03%) | Memory: 67.4 MB (63.15%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let map = {};
+    for(let i=0;i<strs.length;i++){
+        let str = strs[i].split("").sort().join("");
+        if(map[str] != undefined){
+            map[str] = [...map[str], strs[i]]
+        }else{
+            map[str] = [strs[i]]
+        }
+    }
+    let res = [];
+    for(let arr in map){
+        res.push(map[arr])
+    };
+    return res;
+};
