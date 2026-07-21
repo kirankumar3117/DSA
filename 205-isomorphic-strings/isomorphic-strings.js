0cf029d65d036a54f9b75a9aeac3375280891560
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-21 21:24:39 +0530

    Time: 10 ms (16.56%) | Memory: 54.9 MB (59.30%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.js
+++ 205-isomorphic-strings/isomorphic-strings.js
@@ -4,27 +4,28 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    // if(s.length != t.length) return false;
-    // let hash = {};
-    // let nHash = {}
-    // for(let i=0;i<s.length;i++){
-    //     if((hash[s[i]] && hash[s[i]] != t[i]) || (nHash[t[i]] && nHash[t[i]] != s[i])){
-    //        return false;
-    //     }
-    //     hash[s[i]] = t[i];
-    //     nHash[t[i]] = s[i]
-    // };
-    // return true;
-
     if(s.length != t.length) return false;
-    const mapS = {};
-    const mapT = {};
-
-    for (let i = 0; i < s.length; i++) {
-        if (mapS[s[i]] !== mapT[t[i]]) return false;
-        mapS[s[i]] = i + 1;
-        mapT[t[i]] = i + 1;
-    }
-
+    let mapS = {};
+    let mapT = {}
+    for(let i=0;i<s.length;i++){
+        if((s[i] in mapS && mapS[s[i]] != t[i]) ||
+              (t[i] in mapT && mapT[t[i]] != s[i])){
+           return false;
+        }
+        mapS[s[i]] = t[i];
+        mapT[t[i]] = s[i]
+    };
     return true;
+
+    // if(s.length != t.length) return false;
+    // const mapS = {};
+    // const mapT = {};
+
+    // for (let i = 0; i < s.length; i++) {
+    //     if (mapS[s[i]] !== mapT[t[i]]) return false;
+    //     mapS[s[i]] = i + 1;
+    //     mapT[t[i]] = i + 1;
+    // }
+
+    // return true;
 };
