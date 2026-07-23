8ef95d24a0aa9f961463f6cf42317f8f273071f5
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-23 10:40:27 +0530

    Time: 6 ms (20.55%) | Memory: 55.1 MB (68.13%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -5,12 +5,12 @@
 var isValid = function(s) {
     let stack = [];
     let hash = {
-        ')' : '(',
-        '}' : '{',
-        ']' : '['
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
     }
     for(let i=0;i<s.length;i++){
-        if(stack.length > 0 && stack[stack.length-1] == hash[s[i]]){
+        if(stack.length > 0 && hash[stack[stack.length-1]] == s[i]){
             stack.pop()
         }else{
             stack.push(s[i])
