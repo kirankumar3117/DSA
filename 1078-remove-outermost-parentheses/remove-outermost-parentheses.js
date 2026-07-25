692f45d48da9c9b5cb41fada71cae262b89d8763
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-25 21:37:16 +0530

    Time: 4 ms (61.28%) | Memory: 56.6 MB (50.05%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -3,19 +3,31 @@
  * @return {string}
  */
 var removeOuterParentheses = function(s) {
-    let stack = [];
+    // let stack = [];
+    // let str = "";
+    // for(let i=0;i<s.length;i++){
+    //     if(s[i] === "("){
+    //         stack.push(s[i]);
+    //         if(stack.length > 1){
+    //             str = str + s[i]
+    //         }
+    //     }else{
+    //         if(stack.length > 1){
+    //             str = str + s[i]
+    //         }
+    //         stack.pop();
+    //     }
+    // }
+    // return str;
+    let level = 0;
     let str = "";
     for(let i=0;i<s.length;i++){
         if(s[i] === "("){
-            stack.push(s[i]);
-            if(stack.length > 1){
-                str = str + s[i]
-            }
+            level++;
+            if(level > 1) str = str + s[i]
         }else{
-            if(stack.length > 1){
-                str = str + s[i]
-            }
-            stack.pop();
+            if(level > 1) str = str + s[i];
+            level--;
         }
     }
     return str;
