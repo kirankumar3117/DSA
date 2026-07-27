779c632e9bbc3c589bddfff4cd579b7c8d210974
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-27 23:51:30 +0530

    Time: 16 ms (68.82%) | Memory: 79.4 MB (45.32%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -3,14 +3,14 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    let hash = {};
+    let ans = new Array(temperatures.length).fill(0);
     let stack = [];
     for(let i=temperatures.length-1; i>=0; i--){
         while(stack.length && temperatures[stack[stack.length-1]] <= temperatures[i]){
             stack.pop();
         };
-        hash[i] = stack.length ? stack[stack.length-1] - i : 0;
+        ans[i] = stack.length ? stack[stack.length-1] - i : 0;
         stack.push(i) 
     };
-   return Object.values(hash)
+   return ans;
 };
