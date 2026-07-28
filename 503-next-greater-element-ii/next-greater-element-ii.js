9c90dfad64cfb5ff47305fae0e9df433ad7932fa
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-07-28 21:23:58 +0530

    Time: 11 ms (31.61%) | Memory: 64.4 MB (9.94%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.js
+++ 503-next-greater-element-ii/next-greater-element-ii.js
@@ -4,15 +4,15 @@
  */
 var nextGreaterElements = function(nums) {
     const n = nums.length;
-   nums = [...nums, ...nums];
-    const hash = {}
+    nums = [...nums, ...nums];
+    const ans = Array(nums.length).fill(-1);
     const stack = [];
     for(let i=nums.length-1;i>=0;i--){
         while(stack.length && stack[stack.length-1] <= nums[i]){
             stack.pop();
         };
-        hash[i] = stack.length ? stack[stack.length-1] : -1;
+        ans[i] = stack.length ? stack[stack.length-1] : -1;
         stack.push(nums[i])
     };
-    return Object.values(hash).splice(0, n);
+    return ans.splice(0, n);
 };
