3b1d023f45cd34073ef886348da8b5a2a3543db9
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-08-10 00:24:41 +0530

    Time: 0 ms (100.00%) | Memory: 57.6 MB (31.93%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+    let l = 0;
+    let r = nums.length - 1;
+    while(l<r){
+        let m = l + Math.floor((r-l)/2);
+        if(nums[m] != nums[m+1] && nums[m] != nums[m-1]){
+            return nums[m]
+        }else if(nums[m] === nums[m-1]){
+            if((m-2-l)%2 === 0){
+                r= m - 2;
+            }else{
+                l = m + 1;
+            }
+        }else if(nums[m] === nums[m+1]){
+            if((m-1-l) % 2 === 0){
+                r = m - 1
+            }else{
+                l = m + 2
+            }
+        }
+    };
+    return nums[l]
+};
