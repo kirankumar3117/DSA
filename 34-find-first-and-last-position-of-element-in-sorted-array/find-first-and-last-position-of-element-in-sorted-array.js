b76592f5d3e32e23486db676e77dd336f7d40256
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-08-09 22:44:46 +0530

    Time: 0 ms (100.00%) | Memory: 54.8 MB (53.08%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -7,30 +7,29 @@ var searchRange = function(nums, target) {
     let res = [-1, -1]
     let l = 0;
     let r = nums.length - 1;
-    while(l < r){
+    while(l <= r){
         let m = l + Math.floor((r-l)/2);
-        if(nums[m] >= target){
-            r = m;
+        if(nums[m] > target){
+            r = m - 1;
+        }else if(nums[m] < target){
+            l = m + 1;
         }else{
+            res[0] = m;
+            r = m - 1
+        }
+    }
+    l = 0;
+    r = nums.length - 1;
+    while(l <= r){
+        let m = l + Math.ceil((r-l)/2);
+        if(nums[m] > target){
+            r = m - 1
+        }else if(nums[m] < target){
+            l = m + 1
+        }else{
+            res[1] = m;
             l = m + 1;
         }
-    }
-    if(nums[r] === target){
-        res[0] = r
-    }
-    // last one
-    let ll = 0;
-    let rr = nums.length-1;
-    while(ll < rr){
-        let m = ll + Math.ceil((rr-ll)/2);
-        if(nums[m] > target){
-           rr = m - 1;
-        }else{
-            ll = m;
-        }
     };
-    if(nums[ll] === target){
-        res[1] = ll;
-    }
     return res;
 };
