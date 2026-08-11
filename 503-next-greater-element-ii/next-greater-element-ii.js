97d7b98f876833f20617caa5561217f39dc8bd58
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-08-11 16:29:00 +0530

    Time: 5 ms (86.20%) | Memory: 61.8 MB (70.45%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.js
+++ 503-next-greater-element-ii/next-greater-element-ii.js
@@ -3,15 +3,17 @@
  * @return {number[]}
  */
 var nextGreaterElements = function(nums) {
-    let n = nums.length
-    const ans = Array(n).fill(-1);
+    const result = new Array(nums.length).fill(-1);
     const stack = [];
-    for(let i=(2*n)-1;i>=0;i--){
-        while(stack.length && stack[stack.length-1] <= nums[i%n]){
-            stack.pop();
-        };
-        ans[i%n] = stack.length ? stack[stack.length-1] : -1;
+    const n = nums.length;
+    for(let i = (nums.length * 2) -1;i>=0;i--){
+        while(stack.length && stack.at(-1) <= nums[i%n]){
+            stack.pop()
+        }
+        if(stack.length){
+            result[i%n] = stack.at(-1)
+        }
         stack.push(nums[i%n])
     };
-    return ans;
+    return result;
 };
