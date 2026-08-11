b1b73c339000142ae8db41f9c772c18fa6a6a7c4
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-08-11 09:26:02 +0530

    Time: 3 ms (67.98%) | Memory: 58.1 MB (33.59%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -4,14 +4,14 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let map = {};
-    let stack = [];
+    const map = new Map();
+    const stack = [];
     for(let i=nums2.length-1;i>=0;i--){
-        while(stack.length && stack[stack.length-1] < nums2[i]){
+        while(stack.length && stack.at(-1) < nums2[i]){
             stack.pop();
         }
-        map[nums2[i]] = stack.length ? stack[stack.length-1] : -1;
+        map.set(nums2[i], stack.at(-1) ?? -1)
         stack.push(nums2[i])
     };
-    return nums1.map(x => map[x])
+    return nums1.map(x => map.get(x))
 };
