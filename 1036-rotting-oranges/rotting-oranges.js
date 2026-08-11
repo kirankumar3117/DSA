a5464f8f26681bb39a153e8ca2fa307547ebac32
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-08-11 20:42:33 +0530

    Time: 2 ms (97.70%) | Memory: 57 MB (80.91%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.js
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -2,44 +2,46 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var orangesRotting = function(arr) {
-    let m = arr.length;
-    let n = arr[0].length;
-    const queue = []
-    // first find out all the rotten 2's 
-    for(let i = 0; i < m; i++){
-        for( let j = 0; j < n ; j++){
-            if(arr[i][j] === 2){
-                queue.push([i, j, 0])
+var orangesRotting = function(grid) {
+    // find all the rotten oranges
+    const q = []
+    const m = grid.length-1;
+    const n = grid[0].length-1;
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[i].length;j++){
+            if(grid[i][j] === 2){
+                q.push([i,j,0])
             }
         }
     };
     let minMin = 0;
-    // place rotten once in place of 1's
-    while(queue.length){
-        let [i, j, min] = queue.shift();
-        if(j < n-1 && arr[i][j+1] === 1){
-            arr[i][j+1] = 2;
-            queue.push([i, j+1, min+1]);
-        };
-        if(j > 0 && arr[i][j-1] === 1){
-            arr[i][j-1] = 2;
-            queue.push([i, j-1, min+1]);
+    //rotate while q is not empty
+    while(q.length){
+        let [i,j,min] = q.shift();
+        //right
+        if(j <= n && grid[i][j+1] === 1){
+            grid[i][j+1] = 2;
+            q.push([i,j+1,min+1])
         }
-        if(i < m-1 && arr[i+1][j] === 1){
-            arr[i+1][j] = 2;
-            queue.push([i+1, j, min+1]);
+        //left
+        if(j > 0 && grid[i][j-1] === 1){
+            grid[i][j-1] = 2;
+            q.push([i, j-1, min+1])
         }
-        if(i > 0 && arr[i-1][j] === 1){
-            arr[i-1][j] = 2;
-            queue.push([i-1, j, min+1])
-        };
+        if(i < m && grid[i+1][j] === 1){
+            grid[i+1][j] = 2;
+            q.push([i+1, j, min+1])
+        }
+        if( i > 0 && grid[i-1][j] === 1){
+            grid[i-1][j] = 2;
+            q.push([i-1,j,min+1])
+        }
         minMin = Math.max(minMin, min)
-    }
-    // check if any fresh oranges exists
-    for(let i = 0; i < m; i++){
-        for(let j = 0; j < n; j++){
-            if(arr[i][j] === 1){
+    };
+
+    for(let i=0;i<grid.length;i++){
+        for(let j=0;j<grid[i].length;j++){
+            if(grid[i][j] === 1){
                 return -1;
             }
         }
