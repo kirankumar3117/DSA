a4a98d8d072d2f14dae364141a8380e162ac8436
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-08-12 22:00:54 +0530

    Time: 5 ms (81.85%) | Memory: 56.9 MB (83.24%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.js
+++ 1036-rotting-oranges/rotting-oranges.js
@@ -3,48 +3,44 @@
  * @return {number}
  */
 var orangesRotting = function(arr) {
-    // find all the rotten oranges
-    const m = arr.length - 1;
-    const n = arr[0].length - 1;
-    const q = [];
-    for(let i=0;i<=m;i++){
-        for(let j =0;j<=n;j++){
+    let m = arr.length - 1;
+    let n = arr[0].length - 1;
+    const queue = []
+    // first find out all the rotten 2's 
+    for(let i = 0; i <= m; i++){
+        for( let j = 0; j <= n ; j++){
             if(arr[i][j] === 2){
-                q.push([i,j,0])
+                queue.push([i, j, 0])
             }
         }
-    }
+    };
     let minMin = 0;
-    //make all the fresh possible oranges to rotten
-    while(q.length){
-        let [i, j, min] = q.shift();
-        //right
+    // place rotten once in place of 1's
+    while(queue.length){
+        let [i, j, min] = queue.shift();
         if(j < n && arr[i][j+1] === 1){
             arr[i][j+1] = 2;
-            q.push([i, j+1, min+1])
-        }
-        //left
-        if( j > 0 && arr[i][j-1] === 1){
+            queue.push([i, j+1, min+1]);
+        };
+        if(j > 0 && arr[i][j-1] === 1){
             arr[i][j-1] = 2;
-            q.push([i, j-1, min+1]);
+            queue.push([i, j-1, min+1]);
         }
-        //top
-        if(i > 0 && arr[i-1][j] === 1){
-            arr[i-1][j] = 2;
-            q.push([i-1, j, min+1]);
-        }
-        //bottom
         if(i < m && arr[i+1][j] === 1){
             arr[i+1][j] = 2;
-            q.push([i+1, j, min+1])
+            queue.push([i+1, j, min+1]);
         }
+        if(i > 0 && arr[i-1][j] === 1){
+            arr[i-1][j] = 2;
+            queue.push([i-1, j, min+1])
+        };
         minMin = Math.max(minMin, min)
-    };
-    //check if fresh orenge is there
-    for(let i=0;i<=m;i++){
-        for(let j =0;j<=n;j++){
+    }
+    // check if any fresh oranges exists
+    for(let i = 0; i <= m; i++){
+        for(let j = 0; j <= n; j++){
             if(arr[i][j] === 1){
-                return -1
+                return -1;
             }
         }
     };
