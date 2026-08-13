2fe07ad2b0fa4b3286874aa6345827d69f591d81
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-08-13 22:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (46.98%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -3,29 +3,27 @@
  * @param {number} target
  * @return {number}
  */
-
 var search = function(nums, target) {
     let l = 0;
-    let r = nums.length-1;
+    let r = nums.length - 1;
     while(l<=r){
-        let m = l + Math.floor((r-l)/2);
-        if(nums[m] === target) return m;
-        //left sorted
+        const m = l + Math.floor((r-l)/2);
+        if(nums[m] === target){
+            return m;
+        }
         if(nums[l] <= nums[m]){
-            if(target >= nums[l] && target < nums[m]){
-                r = m - 1;
+            if(target >= nums[l] && target <= nums[m]){
+                r = m - 1
             }else{
                 l = m + 1
             }
-        }
-        //right side
-        else{
-            if(target > nums[m] && target <= nums[r]){
-                l = m + 1
+        }else{
+            if(target >= nums[m] &&  target <= nums[r]){
+                l = m +  1
             }else{
-                r = m - 1;
+                r = m - 1
             }
         }
-    };
+    }
     return -1;
 };
