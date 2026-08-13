9c46909ae5c2f289ba603d145c38fee76841c4af
Kiran Kumar <96107740+kirankumar3117@users.noreply.github.com>
2026-08-14 00:53:58 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (29.78%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -6,11 +6,11 @@ var findPeakElement = function(nums) {
     let l = 0;
     let r = nums.length - 1;
     while(l<=r){
-        let m = l + Math.floor((r-l)/2);
+        const m = l + Math.floor((r-l)/2);
         if(nums[m] < nums[m+1]){
             l = m + 1;
         }else{
-             r = m - 1;
+            r = m - 1;
         }
     };
     return l;
